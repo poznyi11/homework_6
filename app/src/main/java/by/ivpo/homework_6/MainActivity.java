d31861c73cc5d7e40d1d7cb5943c126457d0690c
poznyi11 <poznyi11@gmail.com>
2020-04-23 21:10:47 +0300

    homework_6 add to work branch

---
+++ app/src/main/java/by/ivpo/homework_6/MainActivity.java
@@ -0,0 +1,23 @@
+package by.ivpo.homework_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ConstraintLayout cl = findViewById(R.id.layout);
+//        MyView myView = new MyView(this);
+//        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(150, 150);
+//        myView.setLayoutParams(params);
+//        cl.addView(myView);
+
+    }
+}
